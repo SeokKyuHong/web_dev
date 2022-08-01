c26e974f5029643b6c481a689646b82d9c78ad2a
SeokKyuHong <hongsk5689@gmail.com>
2022-08-01 21:10:43 +0900

    서버py 추가, 폼 추가

--- hello_web/__init__.py
+++ hello_web/__init__.py
@@ -1,22 +1,22 @@
-from flask import Flask, Response, g, make_response
+# from flask import Flask, Response, g, make_response
 
-app = Flask(__name__)
-app.debug = True
+# app = Flask(__name__)
+# app.debug = True
 
-@app.route('/res1')
-def res1():
-    custom_res = Response("Custom Trsponse", 200, {ttt})
-    return make_response(custom_res)
+# @app.route('/res1')
+# def res1():
+#     custom_res = Response("Custom Trsponse", 200, {ttt})
+#     return make_response(custom_res)
 
-# @app.before_request
-# def before_request():
-#     print("before_request!!!")
-#     g.str = '한글'
+# # @app.before_request
+# # def before_request():
+# #     print("before_request!!!")
+# #     g.str = '한글'
 
-@app.route("/gg")
-def helloworld_gg():
-    return "Hello World!" + getattr(g, 'str', '111')
+# @app.route("/gg")
+# def helloworld_gg():
+#     return "Hello World!" + getattr(g, 'str', '111')
 
-@app.route("/")
-def helloworld():
-    return "Hello Flask World!!!!!"
+# @app.route("/")
+# def helloworld():
+#     return "Hello Flask World!!!!!"
