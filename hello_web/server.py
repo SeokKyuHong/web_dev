404b426bb2d36eb7b212fdc9835623c5264e7796
SeokKyuHong <hongsk5689@gmail.com>
2022-08-03 23:02:01 +0900

    게시판 쓰기, 읽기 완료

--- hello_web/server.py
+++ hello_web/server.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from crypt import methods
+from turtle import title
+from flask import Flask, render_template, request, redirect
 from pyngrok import conf, ngrok
 
 app = Flask(__name__)
+
+nextId = 4
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
+]
 
 @app.route("/")
 def hello_world():
@@ -15,8 +24,19 @@ def hello_world():
 def new_link():
     return render_template('finder.html')
 
+@app.route("/create/", methods=['GET','POST'])
+def new_create():
+    if request.method == 'GET':
+        return render_template('create.html')
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        newTopic = {'id': nextId, 'title': title, 'body': body}
+        topics.append(newTopic)
+        url = "/read/" + str(nextId)+'/'
+        nextId = nextId +1
+        return title+','+body
 
-# app.run(debug=True)
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
+
+app.run(port=5001, debug=True)
